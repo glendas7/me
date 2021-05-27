3771088bd0bbc21a9a50e49f941117bc9ecab62e
Glenda <gsalas1@uco.edu>
2021-05-27 02:58:57 -0500

    removed sub headers

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,16 +7,12 @@ export default function Skills() {
     return (
         <div class="skills">
             <div class="content-header" id="skills">Skills</div>
-            <div class="content-sub-header">Proficient:</div>
             <div class="arrow">⮞</div>C++
             <div class="arrow">⮞</div>Java
             <div class="arrow">⮞</div>Python
             <div class="arrow">⮞</div>Javascript (Vanilla, Node.js, React)
-            <p><div class="content-sub-header">Familiar:</div>
-                <div class="arrow">⮞</div>SQL
-                <div class="arrow">⮞</div>PHP</p>
-
-
+            <div class="arrow">⮞</div>SQL
+            <div class="arrow">⮞</div>PHP
         </div >
     );
 
