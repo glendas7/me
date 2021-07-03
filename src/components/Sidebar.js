4022461601c36236580189f542950236df189404
Glenda <gsalas1@uco.edu>
2021-07-03 12:45:57 -0500

    added email, moved resume to top

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import MyPhoto from '../images/self.jpg'
 import Image from 'react-bootstrap/Image'
-import { SiLinkedin, SiGithub } from 'react-icons/si';
+import { SiLinkedin, SiGithub, SiGmail } from 'react-icons/si';
 
 class Sidebar extends Component {
     render() {
@@ -12,14 +12,15 @@ class Sidebar extends Component {
                 </header>
                 <Image id="portrait" src={MyPhoto} />
                 <div class="sidebar-nav">
+                    <a href="https://drive.google.com/file/d/1VJKUfgI-NT50ZPgLE-Ss9rSFHa5LSzKt/view?usp=sharing">Resume</a><br />
                     <a href="#about">About</a><br />
                     <a href="#skills">Skills</a><br />
                     <a href="#projects">Projects</a><br />
                     <a href="#education">Education</a><br />
                     <a href="#extra">For Fun</a><br />
-                    <a href="https://drive.google.com/file/d/1VJKUfgI-NT50ZPgLE-Ss9rSFHa5LSzKt/view?usp=sharing">Resume</a><br />
                     <a href="https://github.com/glendasalas7" target="_blank" rel="noopener noreferrer"><SiGithub /></a>
                     <a href="https://www.linkedin.com/in/g2salas" target="_blank" rel="noopener noreferrer"><SiLinkedin /></a><br />
+                    <a href="mailto:gsalas1@uco.edu"><SiGmail /></a>
                 </div >
             </div>
         )
