3464dcb5ed34c464ef9feaf9dd7523faf374cc63
Glenda <gsalas1@uco.edu>
2022-01-02 00:24:28 -0800

    updated education

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,6 +3,7 @@ export default function Education() {
     return (
         <>
             <div class="content-header" id="education">EDUCATION</div>
+            My education goal is to continue learning about additional disciplines of engineering. I amm currently in school working towards my Software Engineering degree while also taking classes to begin my path towards growing my knowledge of mechanical systems and robotics.
             <div class="content-body">
                 University of Central Oklahoma<br />
                 January 2019 - Present (Est Graduation Date: May 2022)<br />
