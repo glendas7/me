81562afeee5e85749269624ba46326a76eec2ef4
Glenda <gsalas1@uco.edu>
2022-01-02 01:03:29 -0800

    updated education

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,11 +3,11 @@ export default function Education() {
     return (
         <>
             <div class="content-header" id="education">EDUCATION</div>
-            I am currently wrapping up the last few semesters of my Bachelors in Software Engineering.
-            At the same time, I am also taking classes to begin my path towards growing my knowledge of mechanical systems, robotics and artificial intelligence.
-            I have yet to decide if this will be don through aquiring an associates, certification, masters or by just taking supplemental classes.
-            If you have any advice on this I am happy to take any advice I can get on the matter.
-            I am predominantly looking for something that can be taken remotely or in the Reno/Sparks Nevada area.
+            I am currently wrapping up the last few semesters of my Bachelors in Software Engineering.<br/>
+            At the same time, I am also taking classes to begin my path towards growing my knowledge of mechanical systems, robotics and artificial intelligence.<br/>
+            I have yet to decide if this will be don through aquiring an associates, certification, masters or by just taking supplemental classes.<br/>
+            If you have any advice on this I am happy to take any advice I can get on the matter.<br/>
+            I am predominantly looking for something that can be taken remotely or in the Reno/Sparks Nevada area.<br/>
             <div class="content-body">
                 University of Central Oklahoma<br />
                 January 2019 - Present (Est Graduation Date: May 2022)<br />
