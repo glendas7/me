13ed9ff280e0f655b0580e1da67ce171eb69ff18
Glenda <gsalas1@uco.edu>
2022-01-02 00:55:34 -0800

    updated education

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,14 +5,17 @@ export default function Education() {
             <div class="content-header" id="education">EDUCATION</div>
             I am currently wrapping up the last few semesters of my Bachelors in Software Engineering.
             At the same time, I am also taking classes to begin my path towards growing my knowledge of mechanical systems, robotics and artificial intelligence.
+            I have yet to decide if this will be don through aquiring an associates, certification, masters or by just taking supplemental classes.
+            If you have any advice on this I am happy to take any advice I can get on the matter.
+            I am predominantly looking for something that can be taken remotely or in the Reno/Sparks Nevada area.
             <div class="content-body">
                 University of Central Oklahoma<br />
                 January 2019 - Present (Est Graduation Date: May 2022)<br />
-                Bachelors of Science in Software Engineering
+                BS in Software Engineering
                 <br /><br />
                 University of Nevada - Reno<br />
                 August 2021 - Present<br />
-                Bachelor of Science in Engineering<br />
+                BS in Engineering<br />
                 <br />
             </div >
         </>
